e1362d1c39fe427ae583f6eaae4d139083a4c644
Horacio <horacio.delgado23@gmail.com>
2016-02-25 21:47:39 -0500

    the form post request can now be handled, doesnt do much though...

--- src/controllers/newClientController.go
+++ src/controllers/newClientController.go
@@ -16,7 +16,11 @@ func (this *newClientController) handle(w http.ResponseWriter, req *http.Request
 	defer responseWriter.Close()
 	
 	vm := viewmodels.GetNewClient()
-	
+	if req.Method == "POST" {
+		vm.Client.Name = req.FormValue("name")
+		vm.Client.Address = req.FormValue("address")
+		vm.Client.Phone = req.FormValue("phone")
+	}
 	responseWriter.Header().Add("Content-Type", "text/html")
 	this.template.Execute(responseWriter, vm)
 }
